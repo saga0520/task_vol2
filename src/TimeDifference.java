efda889959753ec47d0d98c129d2880d844d1f89
saga <saga0520@example.com>
2018-03-29 22:34:24 +0900

    コメント修正

--- src/TimeDifference.java
+++ src/TimeDifference.java
@@ -13,23 +13,23 @@ import java.util.Scanner;
 public class TimeDifference {
 	/**
 	 * 各都市の時差を出力するメソッド
-	 * @author saga
-	 * @version 1.0
+	 * @param aryCts 入力都市と時差
+	 * @param cts 表示都市の数
 	 */
-	public static void city(ArrayList<String> aryCts,int cts) {
+	public static void city(ArrayList<String> aryCts, int cts) {
 		//表示時刻の形式をHH:mmとする
 		SimpleDateFormat df = new SimpleDateFormat("HH:mm");
 		Calendar cal = Calendar.getInstance();
 		System.out.println();
 		System.out.println(cts);
-		try{
-			for (int n = 0; n < cts ; n++) {
+		try {
+			for (int n = 0; n < cts; n++) {
 				if (n < cts) {
 					//各都市の時差をセット
-					String time = aryCts.get(n*2 + 1 );
+					String time = aryCts.get(n * 2 + 1);
 					int numtime = Integer.parseInt(time);
 					//基準都市の時刻をセット
-					String tm = aryCts.get(cts*2+1);
+					String tm = aryCts.get(cts * 2 + 1);
 					try {
 						Date date = df.parse(tm);
 						cal.setTime(date);
@@ -40,7 +40,7 @@ public class TimeDifference {
 						break;
 					}
 				}
-				if(n > 0) {
+				if (n > 0) {
 					//二番目以降に入力した都市と時刻を出力
 					System.out.print("\n");
 					System.out.print(aryCts.get(n + n) + " " + df.format(cal.getTime()));
@@ -49,8 +49,7 @@ public class TimeDifference {
 					System.out.print(aryCts.get(n) + " " + df.format(cal.getTime()));
 				}
 			}
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			System.out.println("\n" + "入力が不適切です");
 		}
 	}
@@ -62,19 +61,19 @@ public class TimeDifference {
 		int N = 0;
 		try {
 			N = sc.nextInt();
-				for(int i = 0; i < N + 1;  i++) {
-					if (N >=1 && 100 >= N) {
-						Scanner sr = new Scanner(System.in);
-						String line = sr.nextLine();
-						//都市名と時差を半角スペースで区切って配列に分割する
-						String[] strs = line.split(" ");
-						for(String s : strs) {
-							ary.add(s);
-						}
+			for (int i = 0; i < N + 1; i++) {
+				if (N >= 1 && 100 >= N) {
+					Scanner sr = new Scanner(System.in);
+					String line = sr.nextLine();
+					//都市名と時差を半角スペースで区切って配列に分割する
+					String[] strs = line.split(" ");
+					for (String s : strs) {
+						ary.add(s);
 					}
 				}
+			}
 		} catch (IndexOutOfBoundsException e) {
-				System.out.println("上限を超えています");
+			System.out.println("上限を超えています");
 		}
 		//各都市の時差を出力するメソッドを呼び出す
 		city(ary, N);
